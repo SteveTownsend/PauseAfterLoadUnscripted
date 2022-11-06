a521e57e332f4126ae33955317122f52b4018400
SteveTownsend <SteveTownsend0@gmail.com>
2022-11-06 09:30:59 -0500

    Bump Version v1.1.0.0. Migrated to VS2022. Local build fixes for lost HDD.

--- Batch/build-AE-artifacts-rar.bat
+++ Batch/build-AE-artifacts-rar.bat
@@ -1,6 +1,6 @@
 @if exist "S:\github\x64\release AE\Pause After Unload Unscripted AE-x.x.x.x.rar" (del "S:\github\x64\release AE\Pause After Unload Unscripted AE-x.x.x.x.rar")
 
-@"C:\Program Files\WinRAR\rar.exe" a -ep -apSKSE/Plugins "s:/github/x64/release AE/Pause After Unload Unscripted AE-x.x.x.x.rar" "s:/github/x64/release AE/PauseAfterLoadUnscripted.dll"
-@"C:\Program Files\WinRAR\rar.exe" a -ep -apSKSE/Plugins "s:/github/x64/release AE/Pause After Unload Unscripted AE-x.x.x.x.rar" s:/github/PauseAfterLoadUnscripted/Config/PauseAfterLoadUnscripted.ini
+@"C:\Program Files\WinRAR\rar.exe" a -ep -apSKSE/Plugins "j:/github/x64/release AE/Pause After Unload Unscripted AE-x.x.x.x.rar" "j:/github/x64/release AE/PauseAfterLoadUnscripted.dll"
+@"C:\Program Files\WinRAR\rar.exe" a -ep -apSKSE/Plugins "j:/github/x64/release AE/Pause After Unload Unscripted AE-x.x.x.x.rar" j:/github/PauseAfterLoadUnscripted/Config/PauseAfterLoadUnscripted.ini
 
-@"C:\Program Files\WinRAR\rar.exe" a -ep "s:/github/x64/release AE/Pause After Unload Unscripted AE-x.x.x.x.rar" s:/github/PauseAfterLoadUnscripted/LICENSE
+@"C:\Program Files\WinRAR\rar.exe" a -ep "j:/github/x64/release AE/Pause After Unload Unscripted AE-x.x.x.x.rar" j:/github/PauseAfterLoadUnscripted/LICENSE
